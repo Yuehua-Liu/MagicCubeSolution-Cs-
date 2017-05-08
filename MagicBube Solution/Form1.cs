0572d3d9cf4048f45d709ce5900deeb5ffd06eea
劉岳樺 <ffang55tw@gmail.com>
2017-05-09 02:30:31 +0800

    修正顏色方格bug

--- MagicBube Solution/Form1.cs
+++ MagicBube Solution/Form1.cs
@@ -31,33 +31,42 @@ namespace MagicBube_Solution
 
             //下面這段為測試用
             textBox10.Text = i.ToString();
-            if (i[0] == color_B)
+
+            //首先的判斷式必須先確定i是否為空字串,則後面判斷i[0]時會因為讀不到而出錯
+            if (i == "")
+            {
+                f_0.ResetText();
+            }
+
+            //test code
+            /*else if (i[0] == color_B)
             {
                 textBox9.Text = "hk";
                 textBox11.BackColor = Color.Blue;
-            }
-            //這邊主要會卡住原因是因為如果裡面是空字串,則會產生錯誤,而改期他字元就不會
+            }*/
+            //這邊主要會卡住原因是因為如果裡面是空字串,則會產生錯誤,而改其他字元就不會
             
-            /*if (i == color_W)
+            else if (i[0] == color_W)
             {
                 f_0.BackColor = Color.White;
 
-            }*//*else if (i == color_R)
+            }else if (i[0] == color_R)
             {
                 f_0.BackColor = Color.Red;
-            }else if (i == color_G)
+            }else if (i[0] == color_G)
             {
                 f_0.BackColor = Color.Green;
-            }else */if (i[0] == color_B)
+            }else if (i[0] == color_B)
             {
                 f_0.BackColor = Color.Blue;
-            }/*else if (i == color_Y)
+            }else if (i[0] == color_Y)
             {
                 f_0.BackColor = Color.Yellow;
-            }else if (i == color_O)
+            }else if (i[0] == color_O)
             {
                 f_0.BackColor = Color.Orange;
-            }*/
+            }
+            
             
         }
 
